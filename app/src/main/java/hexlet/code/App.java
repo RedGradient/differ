f9a4093597db1ee494965de000a478619aa87ddd
redgradient <neutral1shade@yahoo.com>
2022-10-26 21:32:20 +0400

    added 'picocli' dependency; added standard help options

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+//    @Parameters(index = "0", description = "descr1")
+//    private File file1;
+//
+//    @Parameters(index = "1", description = "descr2")
+//    private File file2;
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
+    public static void main(String... args) {
+        new CommandLine(new App()).execute(args);
+//        System.exit(exitCode);
     }
 }
