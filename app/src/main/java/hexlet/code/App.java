e69aca735204e00de0d0eb64d9f1b9e9adba45ca
redgradient <neutral1shade@yahoo.com>
2022-10-29 23:02:59 +0400

    added parameters support; added logic for comparing files from terminal

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,22 +3,34 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
-import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
-//    @Parameters(index = "0", description = "descr1")
-//    private File file1;
-//
-//    @Parameters(index = "1", description = "descr2")
-//    private File file2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Parameters(index = "0", description = "descr1")
+    private String filePath1;
+
+    @Parameters(index = "1", description = "descr2")
+    private String filePath2;
 
     @Override
     public Integer call() throws Exception {
+        System.out.println("in try block\n");
+
+        var text1 = Differ.parse(filePath1);
+        var text2 = Differ.parse(filePath2);
+
+        var diff = Differ.differ(text1, text2);
+
+        System.out.println(diff);
+
         return 0;
     }
 
