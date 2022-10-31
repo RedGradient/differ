92799d93077bf6647a9ac36c80ac7ac3d01d6709
redgradient <neutral1shade@yahoo.com>
2022-10-31 19:09:06 +0400

    added test class and fixtures

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,39 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DifferTest {
+    @Test
+    public static void testDiff() {
+//        String dir = System.getProperty("user.dir");
+//
+//        String content1 = null;
+//        String content2 = null;
+//        try {
+//            content1 = Differ.parse(dir + "/file1.json");
+//            content2 = Differ.parse(dir + "/file2.json");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            assert false;
+//        }
+//
+//        String result = null;
+//        try {
+//            result = Differ.differ(content1, content2);
+//        } catch (Exception e) {
+//            assert false;
+//        }
+//
+//        var expected = """
+//                  - follow: false
+//                    host: hexlet.io
+//                  - proxy: 123.234.53.22
+//                  - timeout: 50
+//                  + timeout: 20
+//                  + verbose: true""";
+
+//        assertEquals(result, expected);
+        assertEquals(2, 3);
+    }
+}
