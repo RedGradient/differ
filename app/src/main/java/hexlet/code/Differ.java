49424f53aeffd011203a52db0636cf904ecea859
redgradient <neutral1shade@yahoo.com>
2022-11-02 18:42:57 +0400

    change return type of 'generate' function; add test

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,21 +7,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 
 public class Differ {
+
+    private static final int SIGN = 0;
+    private static final int VALUE = 1;
+
     public static String differ(String text1, String text2) throws Exception {
         var diff = generate(text1, text2);
 
         StringBuilder builder = new StringBuilder("{\n");
         for (var field : diff.keySet()) {
             var changes = diff.get(field);
-            for (var sign : changes.keySet()) {
-                var value = changes.get(sign);
+            for (var change : changes) {
+                var sign = change[SIGN];
+                var value = change[VALUE];
                 var line = String.format("  %s %s: %s\n", sign, field, value);
                 builder.append(line);
             }
@@ -40,7 +45,7 @@ public class Differ {
         return Files.readString(path);
     }
 
-    public static TreeMap<String, HashMap<String, String>> generate(String text1, String text2)
+    public static TreeMap<String, LinkedList<String[]>> generate(String text1, String text2)
             throws JsonProcessingException {
 
         ObjectMapper objectMapper = new ObjectMapper();
@@ -58,23 +63,22 @@ public class Differ {
             fields.add(it2.next());
         }
 
-        TreeMap<String, HashMap<String, String>> result = new TreeMap<>();
-
+        TreeMap<String, LinkedList<String[]>> result = new TreeMap<>();
         for (var field : fields) {
-            HashMap<String, String> values = new HashMap<>();
+            LinkedList<String[]> values = new LinkedList<>();
             result.put(field, values);
             if (!node1.has(field)) {
-                result.get(field).put("+", node2.get(field).toString());
+                result.get(field).add(new String[]{"+", node2.get(field).toString()});
             } else if (!node2.has(field)) {
-                result.get(field).put("-", node1.get(field).toString());
+                result.get(field).add(new String[]{"-", node1.get(field).toString()});
             } else {
                 var value1 = node1.get(field).toString();
                 var value2 = node2.get(field).toString();
                 if (value1.equals(value2)) {
-                    result.get(field).put(" ", node2.get(field).toString());
+                    result.get(field).add(new String[]{" ", node2.get(field).toString()});
                 } else {
-                    result.get(field).put("-", node1.get(field).toString());
-                    result.get(field).put("+", node2.get(field).toString());
+                    result.get(field).add(new String[]{"-", node1.get(field).toString()});
+                    result.get(field).add(new String[]{"+", node2.get(field).toString()});
                 }
             }
         }
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,39 +1,48 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
     @Test
-    public static void testDiff() {
-//        String dir = System.getProperty("user.dir");
-//
-//        String content1 = null;
-//        String content2 = null;
-//        try {
-//            content1 = Differ.parse(dir + "/file1.json");
-//            content2 = Differ.parse(dir + "/file2.json");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            assert false;
-//        }
-//
-//        String result = null;
-//        try {
-//            result = Differ.differ(content1, content2);
-//        } catch (Exception e) {
-//            assert false;
-//        }
-//
-//        var expected = """
-//                  - follow: false
-//                    host: hexlet.io
-//                  - proxy: 123.234.53.22
-//                  - timeout: 50
-//                  + timeout: 20
-//                  + verbose: true""";
+    public void testDiff() {
 
-//        assertEquals(result, expected);
-        assertEquals(2, 3);
+        File resourcesPath = new File("src/test/resources");
+
+        String content1 = null;
+        String content2 = null;
+        var filePath1 = resourcesPath.getAbsolutePath() + "/file1.json";
+        var filePath2 = resourcesPath.getAbsolutePath() + "/file2.json";
+
+        try {
+            content1 = Differ.parse(filePath1);
+            content2 = Differ.parse(filePath2);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert false;
+        }
+
+        String result = null;
+        try {
+            result = Differ.differ(content1, content2);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert false;
+        }
+
+        var expected = """
+                {
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
+                }""";
+
+        assertEquals(result, expected);
     }
 }
