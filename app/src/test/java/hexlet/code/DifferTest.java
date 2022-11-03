537644dae173cbe62cfe3111f03d62d177fdc3be
redgradient <neutral1shade@yahoo.com>
2022-11-03 22:39:32 +0400

    added new test and fixtures; did some refactor

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -7,31 +8,19 @@ import java.io.File;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
-    @Test
-    public void testDiff() {
+    public static String resourcesPath;
 
-        File resourcesPath = new File("src/test/resources");
+    @BeforeAll
+    public static void beforeAll() {
+        resourcesPath = (new File("src/test/resources")).getAbsolutePath();
+    }
 
-        String content1 = null;
-        String content2 = null;
-        var filePath1 = resourcesPath.getAbsolutePath() + "/file1.json";
-        var filePath2 = resourcesPath.getAbsolutePath() + "/file2.json";
+    public void testDiffAbstract(String filePath1, String filePath2) throws Exception {
 
-        try {
-            content1 = Differ.parse(filePath1);
-            content2 = Differ.parse(filePath2);
-        } catch (Exception e) {
-            e.printStackTrace();
-            assert false;
-        }
+        var content1 = Parse.parse(filePath1);
+        var content2 = Parse.parse(filePath2);
 
-        String result = null;
-        try {
-            result = Differ.differ(content1, content2);
-        } catch (Exception e) {
-            e.printStackTrace();
-            assert false;
-        }
+        var result = Differ.differ(content1, content2);
 
         var expected = """
                 {
@@ -47,15 +36,33 @@ public class DifferTest {
     }
 
     @Test
-    public void testDiffWithEmptyText() {
-        String result = null;
-        try {
-            result = Differ.differ("", "");
-        } catch (Exception e) {
-            e.printStackTrace();
-            assert false;
-        }
+    public void testParse() throws Exception {
+        var filePath = resourcesPath + "/json/file1.json";
+        var content = Parse.parse(filePath);
 
-        assertEquals("", result);
+        var expected = """
+                {
+                  "host": "hexlet.io",
+                  "timeout": 50,
+                  "proxy": "123.234.53.22",
+                  "follow": false
+                }""";
+
+        assertEquals(expected, content);
+    }
+
+    @Test
+    public void testDiffJson() throws Exception {
+
+        var jsonFilePath1 = resourcesPath + "/json/file1.json";
+        var jsonFilePath2 = resourcesPath + "/json/file2.json";
+
+        var yamlFilePath1 = resourcesPath + "/yml/file1.yml";
+        var yamlFilePath2 = resourcesPath + "/yml/file2.yml";
+
+        testDiffAbstract(jsonFilePath1, jsonFilePath2);
+        testDiffAbstract(yamlFilePath1, yamlFilePath2);
+
+        assertEquals("", Differ.differ("", ""));
     }
 }
