33e8789c0ace3d7b955a5a593678494ad86b6473
redgradient <neutral1shade@yahoo.com>
2022-11-15 00:50:20 +0400

    make formatName optional argument

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -31,7 +31,9 @@ public class Differ {
         return objectMapper.readTree(file);
     }
 
-    public static String generate(String filePath1, String filePath2, String formatName) throws Exception {
+    public static String generate(String filePath1, String filePath2) throws Exception {
+
+        var formatName = "stylish";
 
         var node1 = getRootNone(filePath1);
         var node2 = getRootNone(filePath2);
@@ -40,4 +42,12 @@ public class Differ {
         return Formatter.render(diff, formatName);
     }
 
+    public static String generate(String filePath1, String filePath2, String formatName) throws Exception {
+
+        var node1 = getRootNone(filePath1);
+        var node2 = getRootNone(filePath2);
+
+        var diff = Tree.build(node1, node2);
+        return Formatter.render(diff, formatName);
+    }
 }
