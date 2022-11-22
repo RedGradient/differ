320ac56a7824b9836714d9c698ab1d315003b55f
redgradient <neutral1shade@yahoo.com>
2022-11-22 05:59:04 +0400

    refactor

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -11,24 +11,16 @@ public class Parser {
     static Map parseJson(String data) throws Exception {
         var objectMapper = new ObjectMapper();
         Map<String, Object> dataMap = objectMapper.readValue(data, HashMap.class);
-
-        HashMap<String, String> result = new HashMap<>();
-        for (var key : dataMap.keySet()) {
-            var value = dataMap.get(key);
-            try {
-                result.put(key, String.valueOf(value));
-            } catch (Exception e) {
-                throw new Exception("Unexpected object type");
-            }
-        }
-
-        return result;
+        return stringifyMapValues(dataMap);
     }
 
     static Map parseYaml(String data) throws Exception {
         var objectMapper = new YAMLMapper();
         Map<String, Object> dataMap = objectMapper.readValue(data, HashMap.class);
+        return stringifyMapValues(dataMap);
+    }
 
+    private static Map stringifyMapValues(Map<String, Object> dataMap) throws Exception {
         HashMap<String, String> result = new HashMap<>();
         for (var key : dataMap.keySet()) {
             var value = dataMap.get(key);
